5346958f88313d7b640526676b9b8627e6648826
lars-n <lars.neumann@elemica.com>
2021-11-08 12:09:39 +0100

    update code to get rid of warnings

--- project/LiftModuleKeys.scala
+++
@@ -1,9 +0,0 @@
-import sbt._
-
-object LiftModuleKeys {
-
-  lazy val liftVersion = SettingKey[String]("Version number of the Lift Web Framework")
-
-  lazy val liftEdition = SettingKey[String]("Lift Edition (short version number to append to artifact name)")
-
-}
